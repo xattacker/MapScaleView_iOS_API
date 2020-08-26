1bbeb0c6b4f08e451178d21e56ebe406618d0134
xattacker <xattacker.tao@gmail.com>
2020-08-26 15:29:55 +0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MapScaleView",
+    platforms: [.iOS(.v10)],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "MapScaleView",
+            targets: ["MapScaleView"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+       // .package(url: "", .upToNextMajor(from: "")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "MapScaleView",
+ 	    path: "MapScaleView_iOS_API/Sources"),
+    ],
+    swiftLanguageVersions: [.v5]
+)
